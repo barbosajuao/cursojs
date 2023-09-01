0535c2004a27a6b26be75c95a77a4d67a18b62ca
barbosajuao <121351231+barbosajuao@users.noreply.github.com>
2023-08-31 21:27:11 -0300

    aaaa

---
+++ aula07/exercicio01.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`a velocidade do seu carro e ${vel} km/h`)
+if(vel > 60){//condicao simples!
+    console.log(`voce utrapassou a velorcidade permitida, MULTADO!`)
+}
+console.log(`dirija sempre usando cinto de seguranca`)
