16fc188185d29f9b52417d531e072517fff621f4
barbosajuao <121351231+barbosajuao@users.noreply.github.com>
2023-09-04 14:59:23 -0300

    aaaaaa

---
+++ aula11/node_a55e4c637e3a6.tmp.js
@@ -0,0 +1,31 @@
+var c = 1
+while(c <= 6){
+    console.log (`passo ${c}`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*console.log (`tudo bem?`)
+console.log (`tudo bem?`)
+console.log (`tudo bem?`)
+console.log (`tudo bem?`)
+console.log (`tudo bem?`)
+console.log (`tudo bem?`)*/
