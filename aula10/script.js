9efbc2136e24c35a05492b6f32a5f8f8595edeb4
barbosajuao <121351231+barbosajuao@users.noreply.github.com>
2023-09-06 15:02:44 -0300

    123ll

--- aula10/script.js
+++
@@ -1,3 +0,0 @@
-function contar(){
-    window.alert(`deu certo`)
-}
---
+++ aula13/array.js
@@ -0,0 +1,15 @@
+/*
+let num =[5,8,2,9,3]
+num[3]=6 nessa linha ele cria mais um elemento e adiciona 3
+num.push(7) aqui ele adiciona em qualquer lugar o de cima vai adicionar no elemento 3 o numero 6
+num.length aqui vou ver o tamanho do elemento do array
+num.sort() vai alinhar em ordem crescente 
+
+*/
+
+
+
+
+let num =[5,8,2,9,3]
+
+console.log(num)
