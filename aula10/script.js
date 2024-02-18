82eb333346dd01ca8f60039c8ff15a304baa0925
barbosajuao <121351231+barbosajuao@users.noreply.github.com>
2024-02-18 16:50:34 -0300

    asasa

---
+++ aula10/script.js
@@ -0,0 +1,47 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var nas = document.getElementById(`txtano`).value;// aqui estou pegando atraves do dom um dado de um imput
+    var sexo = document.querySelector('input[name="radsex"]:checked').value// so consegui pegar os valores faznedo assim 
+    var res = document.getElementById(`res`)
+    var genero = ``
+    
+     if (nas == `` || nas == null ){
+    window.alert(`o campo nascimento esta esta vazio!`)
+    }else{
+        if(sexo == `homem`){
+            genero = `masculino`
+        }else if (sexo == `mulher`){
+            genero =`Feminino` 
+        } 
+        var idade = Math.abs( nas - ano)
+        res.innerHTML = `foi detectado o genero ${genero} com a idade de ${idade}`
+    }
+    
+   
+         
+    
+
+
+
+
+
+
+
+
+
+    
+    
+   
+
+    
+
+
+     
+        
+        
+    
+
+}
+
+
